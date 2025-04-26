c02182cda4e22c96b7da53577abebab87013005b
Alexandru Nechita <alexandru.italia32@gmail.com>
2025-04-27 01:42:26 +0200

    plugins/snacks: init plugin

---
+++ modules/nixvim/plugins/snacks/default.nix
@@ -0,0 +1,29 @@
+{
+  config,
+  self,
+  system,
+  pkgs,
+  ...
+}:
+{
+  plugins = {
+    snacks = {
+      enable = true;
+      package = self.packages.${system}.snacks-nvim;
+
+      settings = {
+        image.enabled = true;
+        indent.enabled = true;
+        scroll.enabled = true;
+        statuscolumn = {
+          enabled = true;
+
+          folds = {
+            open = true;
+            git_hl = config.plugins.gitsigns.enable;
+          };
+        };
+      };
+    };
+  };
+}
