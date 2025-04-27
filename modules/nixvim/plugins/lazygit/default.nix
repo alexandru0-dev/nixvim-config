b29d22d8e2f28bc73cd43dbd0d75fbf8d237f7c5
Alexandru Nechita <alexandru.italia32@gmail.com>
2025-04-27 02:20:41 +0200

    plugins/snacks/picker: move lazygit from telescope to snacks

--- modules/nixvim/plugins/lazygit/default.nix
+++ modules/nixvim/plugins/lazygit/default.nix
@@ -2,19 +2,19 @@
   plugins.lazygit = {
     enable = true;
   };
-
-  extraConfigLua = ''
-    require("telescope").load_extension("lazygit")
-  '';
-
-  keymaps = [
-    {
-      mode = "n";
-      key = "<leader>gg";
-      action = "<cmd>LazyGit<CR>";
-      options = {
-        desc = "LazyGit (root dir)";
-      };
-    }
-  ];
+  #
+  # extraConfigLua = ''
+  #   require("telescope").load_extension("lazygit")
+  # '';
+  #
+  # keymaps = [
+  #   {
+  #     mode = "n";
+  #     key = "<leader>gg";
+  #     action = "<cmd>LazyGit<CR>";
+  #     options = {
+  #       desc = "LazyGit (root dir)";
+  #     };
+  #   }
+  # ];
 }
