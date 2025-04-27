0a18269650677555fc349e0299bd7d630cd19952
Alexandru Nechita <alexandru.italia32@gmail.com>
2025-04-27 02:32:31 +0200

    keymappings: add format toggle keymaps

--- modules/nixvim/keymappings.nix
+++ modules/nixvim/keymappings.nix
@@ -6,6 +6,45 @@
   };
   keymaps =
     let
+      normal =
+        lib.mapAttrsToList
+          (
+            key:
+            { action, ... }@attrs:
+            {
+              mode = "n";
+              inherit action key;
+              options = attrs.options or { };
+            }
+          )
+          ({
+            "<Space>" = {
+              action = "<NOP>";
+            };
+            "<leader>ut" = {
+              action.__raw = ''
+                function ()
+                  -- vim.g.disable_autoformat = not vim.g.disable_autoformat
+                  vim.cmd('FormatToggle!')
+                  vim.notify(string.format("Buffer Autoformatting %s", bool2str(not vim.b[0].disable_autoformat), "info"))
+                end'';
+              options = {
+                desc = "Buffer Autoformatting toggle";
+              };
+            };
+
+            "<leader>uF" = {
+              action.__raw = ''
+                function ()
+                  -- vim.g.disable_autoformat = not vim.g.disable_autoformat
+                  vim.cmd('FormatToggle')
+                  vim.notify(string.format("Global Autoformatting %s", bool2str(not vim.g.disable_autoformat), "info"))
+                end'';
+              options = {
+                desc = "Global Autoformatting toggle";
+              };
+            };
+          });
       visual =
         lib.mapAttrsToList
           (
@@ -45,6 +84,5 @@
             };
           };
     in
-    helpers.keymaps.mkKeymaps { options.silent = true; } (visual);
-  # helpers.keymaps.mkKeymaps { options.silent = true; } (normal ++ visual ++ insert);
+    helpers.keymaps.mkKeymaps { options.silent = true; } (normal ++ visual);
 }
