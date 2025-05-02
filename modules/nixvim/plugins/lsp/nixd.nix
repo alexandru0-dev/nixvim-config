3ced746e0f54f50ddca11960aff1bc6b0388f323
Alexandru Nechita <alexandru.italia32@gmail.com>
2025-05-03 00:11:34 +0200

    plugins/lsp/nixd: fix nixvim completion

--- modules/nixvim/plugins/lsp/nixd.nix
+++ modules/nixvim/plugins/lsp/nixd.nix
@@ -6,40 +6,28 @@
 }:
 let
   flake = ''(builtins.getFlake (builtins.toString (builtins.findFile builtins.nixPath "self")))'';
-  system = ''''${builtins.currentSystem}'';
-  # flake = ''builtins.getFlake (builtins.toString (builtins.findFile builtins.nixPath "self"))'';
   hostname = ''''${(${flake}.inputs.nixpkgs.lib.trim (builtins.readFile "/etc/hostname"))}'';
 in
 {
   plugins.lsp.servers.nixd = {
     enable = true;
-    settings =
-      # nixpkgs.expr = ''(builtins.getFlake "/etc/nixos").inputs.nixpkgs {}'';
+    settings = {
+      nixpkgs.expr = "import ${flake}.inputs.nixpkgs { }";
+      formatting.command = [ "${lib.getExe pkgs.nixfmt-rfc-style}" ];
+      options = rec {
+        # Completitions for nixos and home manager options
+        nixos.expr = ''${flake}.nixosConfigurations.${hostname}.options'';
+        home_manager.expr = ''${nixos.expr}.home-manager.users.type.getSubOptions [ ]'';
+        # TODO: filter everything not doc releated
+        nixvim.expr = ''((builtins.getFlake "${self}").inputs.nixvim.lib.evalNixvim { system = builtins.currentSystem; }).options'';
 
-      {
-        nixpkgs.expr = "import ${flake}.inputs.nixpkgs { }";
-        formatting.command = [ "${lib.getExe pkgs.nixfmt-rfc-style}" ];
-        options = rec {
-          # Completitions for nixos and home manager options
-          nixos.expr = ''${flake}.nixosConfigurations.${hostname}.options'';
-          home_manager.expr = ''${nixos.expr}.home-manager.users.type.getSubOptions [ ]'';
-          nixvim.expr = ''
-            (
-              builtins.fromJSON (
-                builtins.readFile "''${
-                  (builtins.getFlake "${self}")
-                  .inputs.nixvim.outputs.packages.''${builtins.currentSystem}.options-json
-                }/share/doc/nixos/options.json"
-              )
-            )'';
-
-          # For flake-parts options.
-          # Firstly read the docs here to enable "debugging", exposing declarations for nixd.
-          # https://flake.parts/debug
-          flake-parts.expr = ''(builtins.getFlake "${self}").debug.options'';
-          # For a `perSystem` flake-parts option:
-          flake-parts2.expr = ''(builtins.getFlake "${self}").currentSystem.options'';
-        };
+        # For flake-parts options.
+        # Firstly read the docs here to enable "debugging", exposing declarations for nixd.
+        # https://flake.parts/debug
+        flake-parts.expr = ''(builtins.getFlake "${self}").debug.options'';
+        # For a `perSystem` flake-parts option:
+        flake-parts2.expr = ''(builtins.getFlake "${self}").currentSystem.options'';
       };
+    };
   };
 }
