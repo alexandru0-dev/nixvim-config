2222a701fb2163f36cfcb3a61448fa7bdf1dfcce
Alexandru Nechita <alexandru.italia32@gmail.com>
2025-09-24 11:57:55 +0200

    plugins/lsp/python: enable ruff lsp server and enable some analysis option in basedpyright

--- modules/nixvim/plugins/lsp/python.nix
+++ modules/nixvim/plugins/lsp/python.nix
@@ -1,12 +1,17 @@
-{ ... }:
 {
   plugins.lsp.servers.basedpyright = {
     enable = true;
     settings = {
+      analysis.analysis.useTypingExtensions = true;
+      analysis.inlayHints.genericTypes = true;
       analysis.diagnosticSeverityOverrides = {
         reportCallInDefaultInitializer = "none";
       };
     };
+  };
+
+  plugins.lsp.servers.ruff = {
+    enable = true;
   };
 
 }
