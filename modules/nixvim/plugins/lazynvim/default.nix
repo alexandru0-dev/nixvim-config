6893d989ea71b91bfda30a62d158de3f7639974a
Alexandru Nechita <alexandru.italia32@gmail.com>
2025-11-12 11:37:34 +0100

    wip

--- modules/nixvim/plugins/lazynvim/default.nix
+++ modules/nixvim/plugins/lazynvim/default.nix
@@ -1,12 +1,34 @@
-{pkgs, ...}:
+{ pkgs, lib, ... }:
 {
   plugins.lazy = {
-    enable = true;
-    plugins = with pkgs.vimPlugins [
-    showkeys
+    enable = false;
+    plugins = [
+      # showkeys
+      # Load on a combination of conditions: specific filetypes or commands
+      {
+        pkg = pkgs.emptyFile;
+        name = "nvzone/showkeys";
+        dir = "${lib.mkForce ./default.nix}";
+        dev = true;
+        cmd = "ShowkeysToggle";
 
+        # name = "w0rp/ale";
+        # ft = [
+        #   "sh"
+        #   "zsh"
+        #   "bash"
+        #   "c"
+        #   "cpp"
+        #   "cmake"
+        #   "html"
+        #   "markdown"
+        #   "racket"
+        #   "vim"
+        #   "tex"
+        # ];
+        # cmd = "ALEEnable";
+      }
     ];
 
   };
-  # plugins.lazy
 }
