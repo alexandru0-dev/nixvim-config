043e89d8b32b425fae66c5120975c0c83afa0279
Alexandru Nechita <alexandru.italia32@gmail.com>
2025-12-09 10:42:26 +0100

    plugins/lualine: add info about recording macro

--- modules/nixvim/plugins/lualine/default.nix
+++ modules/nixvim/plugins/lualine/default.nix
@@ -1,5 +1,46 @@
+{ config, lib, ... }:
 {
   plugins.lualine = {
     enable = true;
+
+    lazyLoad.settings.event = [
+      "VimEnter"
+      "BufReadPost"
+      "BufNewFile"
+    ];
+
+    settings.sections = {
+
+      lualine_c = [
+        {
+          __unkeyed-1.__raw = ''
+            function()
+              local reg = vim.fn.reg_recording()
+              if reg ~= "" then
+                return "Recording @" .. reg
+              else
+                return ""
+              end
+            end
+          '';
+          cond.__raw = ''
+            function()
+              local reg = vim.fn.reg_recording()
+              if reg ~= "" then
+                return true
+              else
+                return false
+              end
+            end
+          '';
+          color = {
+            fg = "#ff9e64";
+          };
+        }
+      ];
+    };
+    settings.options = {
+      refresh.statusline = 100;
+    };
   };
 }
